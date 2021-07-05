55962ddfceeaaffe7ee06247a2d5ab7e8a6889cf
MuzykaAndrii <andrymyzik@gmail.com>
2021-07-06 01:55:11 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask, request
+
+import telebot
+
+TOKEN = os.getenv('BOT_TOKEN')
+WEBHOOK = os.getenv('WEBHOOK')
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
